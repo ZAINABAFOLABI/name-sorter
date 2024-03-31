dbf4ca4df06828435a9be6882cf67aa38fe6aa74
ZAINABAFOLABI <zainab.o.afolabi@gmail.com>
2024-03-31 06:18:51 +0100

    Add project files.

---
+++ name-sorter.consoleapp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace name_sorter.consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
